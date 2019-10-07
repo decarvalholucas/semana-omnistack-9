96f331c2a8c550a6e82e573c4868e047862ac059
Lucas de Carvalho Alves <lucasdecarvalho.ti@gmail.com>
2019-10-07 02:17:44 -0300

    Omnistack Week Finished

--- aulas/backend/src/server.js
+++ aulas/backend/src/server.js
@@ -3,13 +3,34 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 const path = require('path')
 
+const socketio = require('socket.io')
+const http = require('http')
+
 const routes = require('./routes')
 
 const app = express()
+const server = http.Server(app)
+const io = socketio(server)
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack-t4sa9.mongodb.net/semana09?retryWrites=true&w=majority',{
   useNewUrlParser: true,
   useUnifiedTopology: true
+})
+
+const connectedUsers = {}
+
+io.on('connection', socket => {
+  const { user_id } = socket.handshake.query
+  connectedUsers[user_id] = socket.id
+
+  console.log(connectedUsers)
+
+})
+
+app.use((req, res, next) => {
+  req.io = io
+  req.connectedUsers = connectedUsers
+  return next()
 })
 
 app.use(cors())
@@ -18,4 +39,4 @@ app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')))
 app.use(routes)
 
 
-app.listen(3333)
+server.listen(3333)
